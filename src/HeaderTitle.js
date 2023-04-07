0631b9c43553b66383c8d16909c805f70f100f5f
Francisco Javier Giannico Villalobos <fjgiannico@gmail.com>
2023-04-06 21:09:29 -0300

    corrección altura header

--- src/HeaderTitle.js
+++ src/HeaderTitle.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const HeaderTitle = () => {
   return (
-    <div>
+    <div className='header'>
         <h1>Blackdolar.com</h1>
         <h5>Todas las cotizaciones del dolar al instante</h5>
     </div>
